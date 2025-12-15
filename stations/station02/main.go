83525e7b6c6ef1a7df737c3aeeaff13668bdd11a
tyz8250 <tyz0308@gmail.com>
2025-12-15 20:39:53 +0900

    count-delete

--- stations/station02/main.go
+++ stations/station02/main.go
@@ -6,10 +6,13 @@ func main() {
 	x, y := 1, 2
 
 	fmt.Println("x: ", x, "y: ", y) // 期待される出力: 「x: 1, y: 2」
-
 	Swap(&x, &y)
-
-	fmt.Println("x: ", x, "y: ", y) // 期待される出力: 「x: 2, y: 1」
 }
 
-func Swap() {}
+func Swap(x, y *int) {
+	*x, *y = *y, *x
+	fmt.Println("x: ", *x, "y: ", *y) // 期待される出力: 「x: 2, y: 1」
+}
+
+
+
